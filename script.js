f94a811ab7d390cbe2d8d290beb46ac2def6d12b
Anthony Ray <rayanthony42@gmail.com>
2023-12-04 06:04:43 -0500

    Finshed

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const h2 = document.createElement("h2");
-h2.textContent = "There can be no limits";
+var h2 = document.createElement("h2");
+
+h2.textContent = "No More Limits!";
+
+document.querySelector("body").appendChild(h2);
+
+h2.addEventListener("mouseover", function() {
+    h2.style.backgroundColor = "black";
+});
+
+h2.addEventListener("mouseout", function() {
+    h2.style.backgroundColor = "";
+});
